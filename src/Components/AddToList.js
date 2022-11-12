272a40650fcbc9470e55415b9825a21b0bbe6e61
Urvashi Roy <urvashiroy1996@gmail.com>
2022-11-13 04:59:30 +0530

    React Assignment

---
+++ src/Components/AddToList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AddToList = () => {
+    return (
+        <>
+            <div className="mylist">
+                <p>Add To Cart +</p>
+            </div>
+        </>
+    )
+}
